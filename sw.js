e5b4ab35aa2ac43b5000708fb8454b268c5176b3
Elvis <amalanathan.elvis@gmail.com>
2023-03-22 13:15:07 +0100

    Initial commit of PWA files

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          'https://rawgit.com/schmich/instascan-builds/master/instascan.min.js',
+          'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
